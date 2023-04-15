804b9fdc494c7caae1c1fb61e563aadcf3e9cd71
PytoByte <78215689+PytoByte@users.noreply.github.com>
2023-04-15 18:46:53 +0700

    Broking code
    none

--- app/src/main/java/com/example/psasic/LoginAPI.java
+++ app/src/main/java/com/example/psasic/LoginAPI.java
@@ -2,13 +2,8 @@ package com.example.psasic;
 
 import org.json.JSONObject;
 
-<<<<<<< HEAD
 import okhttp3.ResponseBody;
 import retrofit2.Call;
-=======
-import retrofit2.Call;
-import retrofit2.Response;
->>>>>>> origin/main
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
@@ -17,9 +12,5 @@ public interface LoginAPI {
 
     @POST("/register")
     @FormUrlEncoded
-<<<<<<< HEAD
     Call<ResponseBody> register(@Field("username") String username, @Field("password") String password );
-=======
-    Call<Response> register(@Field("username") String username, @Field("password") String password );
->>>>>>> origin/main
 }
--- app/src/main/java/com/example/psasic/MainActivity.java
+++ app/src/main/java/com/example/psasic/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.psasic;
 
-<<<<<<< HEAD
 import androidx.annotation.NonNull;
-=======
->>>>>>> origin/main
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -14,10 +11,7 @@ import com.example.psasic.databinding.ActivityMainBinding;
 
 import org.json.JSONObject;
 
-<<<<<<< HEAD
 import okhttp3.ResponseBody;
-=======
->>>>>>> origin/main
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -34,27 +28,15 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-<<<<<<< HEAD
         LoginService.getInstance().register("БЛЯТЬ", "РАБОТАЙ СУКА").enqueue(new Callback<ResponseBody>() {
             @Override
             public void onResponse(@NonNull Call<ResponseBody> call, @NonNull Response<ResponseBody> response) {
-                Log.e("TAG", "onResponse: "+response.body());
-            }
-
-            @Override
-            public void onFailure(@NonNull Call<ResponseBody> call, @NonNull Throwable t) {
-                Log.e("TAG", "onFailure: "+t);
-=======
-        LoginService.getInstance().register("БЛЯТЬ", "РАБОТАЙ СУКА").enqueue(new Callback<Response>() {
-            @Override
-            public void onResponse(Call<Response> call, Response<Response> response) {
                 Log.d("TAG", "onResponse: "+response.body().toString());
             }
 
             @Override
-            public void onFailure(Call<Response> call, Throwable t) {
+            public void onFailure(@NonNull Call<ResponseBody> call, @NonNull Throwable t) {
                 Log.d("TAG", "onFailure: "+t);
->>>>>>> origin/main
             }
         });
     }
